887c39123a42543948a71c2256d63ece236c4662
Cameron Clough <cameronjclough@gmail.com>
2022-11-28 22:40:06 -0800

    Revert "faster"
    This reverts commit 5a3dc39c331e57d97119a49f015f38f18d913c2d.

--- system/micd.py
+++ system/micd.py
@@ -22,7 +22,7 @@ class Mic:
     self.rk = Ratekeeper(RATE)
 
     self.measurements = np.array([])
-    self.filter = FirstOrderFilter(1, 2, DT_MIC)
+    self.filter = FirstOrderFilter(1, 3, DT_MIC)
     self.last_alert_time = 0
 
   def update(self):

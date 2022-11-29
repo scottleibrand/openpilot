fef2cd540374c8b4ad9d357890417a7c15d9413f
Cameron Clough <cameronjclough@gmail.com>
2022-11-28 17:02:34 -0800

    try no device

--- system/micd.py
+++ system/micd.py
@@ -7,7 +7,6 @@ import numpy as np
 from cereal import messaging
 from common.filter_simple import FirstOrderFilter
 from common.realtime import Ratekeeper
-from system.hardware import HARDWARE
 from system.swaglog import cloudlog
 
 RATE = 10
@@ -52,9 +51,6 @@ class Mic:
     self.measurements = np.concatenate((self.measurements, indata[:, 0]))
 
   def micd_thread(self, device=None):
-    if device is None:
-      device = HARDWARE.get_sound_input_device()
-
     with sd.InputStream(device=device, channels=1, samplerate=44100, callback=self.callback) as stream:
       cloudlog.info(f"micd stream started: {stream.samplerate=} {stream.channels=} {stream.dtype=} {stream.device=}")
       while True:
